9a462c192e2c867e99ab843918f989b344fa9dfb
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-09 00:08:20 +0200

    feat: implement github api auth

---
+++ app/src/middleware/auth.js
@@ -0,0 +1,16 @@
+const { createAppAuth } = require("@octokit/auth-app");
+const { Octokit } = require("@octokit/rest");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "src/.env" })
+
+const octokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+        appId: process.env.APP_ID,
+        privateKey: process.env.PRIVATE_KEY,
+        installationId: process.env.INSTALLATION_ID,
+    },
+});
+
+module.exports = { octokit };
