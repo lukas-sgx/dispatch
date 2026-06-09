97e4bb0182a67bd2475723eac4d2a0db37cbe211
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-09 22:59:30 +0200

    feat: add comment on PR creation

--- app/src/pulls/controller.js
+++ app/src/pulls/controller.js
@@ -2,12 +2,19 @@ const comment = require("./actions/comment");
 
 async function handle_new_pr(pull_request, data) {
     try {
-        // await comment.add(
-        //     data.repository.owner.login,
-        //     data.repository.name,
-        //     pull_request.number,
-        //     "Once the "
-        // ); 
+        await comment.add(
+            data.repository.owner.login,
+            data.repository.name,
+            pull_request.number,
+            `Once the PR is merged, the deployment will be triggered automatically. Reviewers will be notified once all CI checks have passed, please make sure:
+
+- [ ] All tests pass locally
+- [ ] The description clearly explains the changes
+- [ ] Related issues are linked (closes #...)
+- [ ] No debug code or TODOs left behind
+
+Thanks for your contribution!`
+        ); 
     } catch (err) {}
 }
 
